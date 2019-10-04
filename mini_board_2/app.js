20e12aa35bca8b1fd70fe63e7f9b9d2531dfa675
darshimo <takumishimoda7623@gmail.com>
2019-10-04 18:24:39 +0900

    fix

--- mini_board_2/app.js
+++ mini_board_2/app.js
@@ -4,8 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* new */
+//var bodyParser = require('body-parser');
+var session = require('express-session');
+//var validator = require('express-validator');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var home = require('./routes/home');
 
 var app = express();
 
@@ -16,26 +22,39 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+/* new */
+//app.use(validator());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var session_opt = {
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 }
+};
+app.use(session(session_opt));
+
+app.use('/users', users);
+app.use('/', index);
+app.use('/home', home);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- mini_board_2/routes/users.js
+++ mini_board_2/routes/users.js
@@ -1,9 +1,115 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* new */
+var { check, validationResult } = require('express-validator');
+
+var mysql = require('mysql');
+
+var knex = require('knex')({
+    dialect: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'my-nodeapp-db',
+        charset: 'utf8'
+    }
+});
+
+var Bookshelf = require('bookshelf')(knex);
+
+var User = Bookshelf.Model.extend({
+    tableName: 'users'
+});
+
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Users/Add',
+        form: { name: '', password: '', comment: '' },
+        content: 'input name,password,comment'
+    }
+    res.render('users/add', data);
+});
+
+router.post('/add', [
+    check('name', 'input NAME').not().isEmpty(),
+    check('password', 'input PASSWORD').not().isEmpty(),
+], (req, res, next) => {
+    var result = validationResult(req);
+    if (!result.isEmpty()) {
+        var content = '<ul class="error">';
+        var result_arr = result.array();
+        for (var n in result_arr) {
+            content += '<li>' + result_arr[n].msg + '</li>'
+        }
+        content += '</ul>';
+        var data = {
+            title: 'Users/Add',
+            content: content,
+            form: req.body
+        }
+        res.render('users/add', data);
+    } else {
+        req.session.login = null;
+        new User(req.body).save().then((model) => {
+            res.redirect('/');
+        });
+    }
+});
+
+router.get('/', (req, res, next) => {
+    var data = {
+        title: 'Users/Login',
+        form: { name: '', password: '' },
+        content: 'input name, password'
+    }
+    res.render('users/login', data);
+});
+
+router.post('/', [
+    check('name', 'input NAME').not().isEmpty(),
+    check('password', 'input PASSWORD').not().isEmpty(),
+], (req, res, next) => {
+    var result = validationResult(req);
+    if (!result.isEmpty()) {
+        var content = '<ul class="error">';
+        var result_arr = result.array();
+        for (var n in result_arr) {
+            content += '<li>' + result_arr[n].msg + '</li>'
+        }
+        content += '</ul>';
+        var data = {
+            title: 'Users/Login',
+            content: content,
+            form: req.body
+        }
+        res.render('users/login', data);
+    } else {
+        var nm = req.body.name;
+        var pw = req.body.password;
+        console.log("check");
+        User.query({ where: { name: nm }, andWhere: { password: pw } })
+            .fetch()
+            .then((model) => {
+                console.log("ok");
+                req.session.login = model.attributes;
+                var data = {
+                    title: 'Users/Login',
+                    content: '<p>You logined<br>back to toppage and send message</p>',
+                    form: req.body
+                };
+                res.render('users/login', data);
+            }).catch((err) => {
+                console.log("ng");
+                var data = {
+                    title: 're-input',
+                    content: '<p class="error">name or password is not correct</p>',
+                    form: req.body
+                };
+                res.render('users/login', data);
+            });
+    }
 });
 
 module.exports = router;
